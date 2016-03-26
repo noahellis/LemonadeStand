b376468eb44f97cc7fb638198763124117c5ee4f
noahellis <noah_ellis@hotmail.com>
2016-03-26 14:01:09 -0500

    created class to track inventory throughout day as well as add money to user's money after sales

---
+++ LemonadeSales.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class LemonadeSales
+    {
+        Customer purchasingCustomer = new Customer()
+        public void SubtractIngredients()
+        {
+            //subtract ingredients for each buying customer (ingredient type starting -(sales * ingredient type))
+            //need to add code to end the day if user runs out of any kind of ingredient
+            //decrement ingredients until one reaches 0 or all customers go through line
+        }
+
+        public void AddMoney()
+        {
+            //add sales from variable inputted by user, multiplied by number of sales
+        }
+
+        
+    }
+}
